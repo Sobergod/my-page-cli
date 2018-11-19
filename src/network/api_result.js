d6fb3ae85327442c204ba34329f584dc3762bb0b
e <sober_e1@qq.com>
2018-11-19 11:52:17 +0800

    重构网络请求

--- src/network/api_result.js
+++ src/network/api_result.js
@@ -1,9 +1,10 @@
 import netWork from "./network_config"
 const get = netWork.getResult,
     post = netWork.postResult;
+const HOME = "/home";
 class ApiResult {
     test(params) {
-        return get("/home", params);
+        return get(HOME, params);
     }
 }
 export default new ApiResult();
